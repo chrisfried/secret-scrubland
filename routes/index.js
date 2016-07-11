dc3f0442e7b8ab5ffbffed7ab158da5587b9fd25
Christopher Fried <Christopher.Fried@infor.com>
2016-07-11 09:01:32 -0500

    request

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var httpProxy = require('http-proxy');
+var request = require('request');
 var fs = require('fs');
 
 router.get('/', function (req, res) {
@@ -13,27 +13,21 @@ router.get('/:membershipType/:guardian/fixed', function (req, res) {
   res.render('guardian');
 });
 
-var bungieProxy = httpProxy.createProxyServer({
-  target: 'http://www.bungie.net',
-  autoRewrite: true
+router.get('/Platform/*?', function (req, res) {
+  res.setTimeout(25000);
+  var options = {
+    url: 'https://www.bungie.net/' + req.originalUrl,
+    headers: {'X-API-Key': process.env.BUNGIE_KEY}
+  };
+  try {
+    request(options, function (error, response, body) {
+      if (!error) {
+        res.send(body);
+      } else {
+        res.send(error);
+      }
+    });
+  } catch (e) {}
 });
-
-bungieProxy.on('proxyReq', function(proxyReq, req, res) {
-  proxyReq.setHeader('host', 'www.bungie.net');
-  proxyReq.setHeader('x-api-key', process.env.BUNGIE_KEY);
-});
-
-bungieProxy.on('error', function(err, req, res) {
-  console.log(err);
-  res.writeHead(500, {
-    'Content-Type': 'text/plain'
-  });
-
-  res.end('Something went wrong contacting Bungie.');
-});
-
-router.get('/Platform*', function (req, res) {
-  bungieProxy.web(req, res);
-})
 
 module.exports = router;
