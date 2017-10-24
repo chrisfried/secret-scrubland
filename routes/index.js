27e81b914a5bc9681d69939b739424263fc2028b
Chris Fried <fried.chris@gmail.com>
2017-10-24 14:32:19 -0500

    Redirect to new site.

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,13 @@ var request = require('request');
 var fs = require('fs');
 
 router.get('/', function (req, res) {
-  res.render('index');
+  res.redirect('https://chrisfried.github.io/secret-scrublandeux/');
 });
 router.get('/:membershipType/:guardian', function (req, res) {
-  res.render('guardian');
+  res.redirect('https://chrisfried.github.io/secret-scrublandeux/');
 });
 router.get('/:membershipType/:guardian/fixed', function (req, res) {
-  res.render('guardian');
-});
-
-// Courtesy of https://github.com/DestinyTrialsReport/DestinyTrialsReport/blob/05c113f8d39dee2a02461902f0c9e1c287cad3aa/server.js#L37
-router.get('/d1/Platform/*?', function (req, res) {
-  res.setTimeout(25000);
-  var options = {
-    url: 'https://www.bungie.net/' + req.originalUrl,
-    headers: {'X-API-Key': process.env.BUNGIE_KEY}
-  };
-  try {
-    request(options, function (error, response, body) {
-      if (!error) {
-        res.send(body);
-      } else {
-        res.send(error);
-      }
-    });
-  } catch (e) {}
+  res.redirect('https://chrisfried.github.io/secret-scrublandeux/');
 });
 
 module.exports = router;
